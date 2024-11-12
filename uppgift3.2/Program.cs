7b13526a20fd1619b3402521a7899bfa1795ce22
mariu <mariu@DESKTOP-CT70FK7>
2024-11-12 18:32:15 +0100

    Add project files.

---
+++ uppgift3.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Har du gått ut gymnasiet? (j/n): ");
+            string godkänd = Console.ReadLine();
+
+            Console.Write("Hur gammal är du? ");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (godkänd == "j" && ålder < 22)
+            {
+                Console.WriteLine("Vi vill gärna anställa dig");
+            }
+            else
+            {
+                Console.WriteLine("Vi letar tyvärr efter annan personal just nu");
+            }
+        }
+    }
+}
